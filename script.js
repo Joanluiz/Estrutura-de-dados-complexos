53ba61d72a9435147b3d93bf944250592c4b5feb
Joanluiz <joanluiz69@gmail.com>
2022-07-06 19:48:27 -0300

    td certo por aqui

---
+++ script.js
@@ -0,0 +1,39 @@
+ // Crie um array que receba 5 itens e exiba no console.
+/*let items = ["gato", "cachorro", "papagaio", "galinha", "rato"];
+
+console.log(items);
+*/
+
+// Utilize um método para adicionar um nome ao inicio do array.
+/*let semana = ['segunda', 'terça', 'quarta', 'quinta']
+
+semana.unshift('domingo')
+console.log(semana);
+*/
+
+// Utilize um método para remover o último nome do array.
+/*let semana = ['segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado', 'domingo'];
+semana.pop();
+console.log(semana);
+*/
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+/*var paises = ['Brasil', 'Argentina', 'Colombia', 'EUA'];
+paises.push('Japão', 'Coreia do Sul');
+console.log(paises);
+*/
+
+// Utilize um método para remover o primeiro nome do array.
+/*var animais = ['gato', 'cachorro', 'rato'];
+animais.shift();
+console.log(animais);
+*/
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+// let numbers = [7,5,6,3,8,9,2,1,4]
+/*let numbers = [7,5,6,3,8,9,2,1,4]
+
+numbers.sort()
+console.log(numbers)
+*/
+
